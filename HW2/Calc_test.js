748f970e29834cdb87141951e2271656d64cd4e7
ysyang860122 <ysyang860122@gmail.com>
2025-04-08 14:57:36 +0800

    Add tests for ADD and SUBSTRACT, but the part of SUBSTRACT will fail

--- HW2/Calc_test.js
+++ HW2/Calc_test.js
@@ -2,3 +2,16 @@ const test = require('node:test');
 const assert = require('assert');
 const Calc = require('./Calc');
 
+test('測試加法功能', async (t) => {
+    await t.test('兩個正數相加', () => {
+        assert.strictEqual(Calc.add(2, 3), 5);
+    });
+});
+
+test('測試減法功能', async (t) => {
+    await t.test('兩個正數相減', () => {
+        assert.strictEqual(Calc.subtract(5, 3), 2);
+    });
+});
+
+
